bd1cf257a39eea1bb88f645e1980f17bc5fae77d
yveega <47260215+yveega@users.noreply.github.com>
2022-01-08 14:53:55 +0300

    Complete count_keywords_koe() function, but TODO: write add_product_with_koe() function in add_product.py

--- bd_post_processing.py
+++ bd_post_processing.py
@@ -1,13 +1,15 @@
 from SessionManager import SessionManager
 from images import Product
 from keywords import get_keywords_koe
-from clustering_graph_db import ClusterToKeyword
+from tqdm import tqdm
+from add_product import add_product_with_koe
 
 
 def count_keywords_koe():
     total = SessionManager().session().query(Product).all()
-    for id, product in enumerate(total):
+    for id, product in tqdm(enumerate(total)):
         text = product.name + "\n" + product.description
-        cur_keywords = get_keywords_koe(text)
-        ...
+        keywords = get_keywords_koe(text)
+        add_product_with_koe(product.id, product.cost, product.name, product.description, product.reviews_count,
+                             product.avg_rating, keywords)
 
