4220db5aea9bc39a0aa4df5fb8b824d9a26d4183
nilovlev <nilovlev@yandex.ru>
2022-01-09 11:56:18 +0300

    telegram bot small fix

---
+++ bot/consts.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from singleton import singleton
+
+
+@singleton
+@dataclass
+class Messages:
+    HELP_MESSAGE = 'help message...'
+    START_MESSAGE = 'start message...'
+    COMPARE_KEYWORD_START_MESSAGE = 'Start "Compare Keywords" algorithm...'
+
+
+@singleton
+@dataclass
+class Commands:
+    START_COMMAND = 'Start bot'
+    HELP_COMMAND = 'Get Help'
+    KEYWORDS_ALGORITHMS_COMMAND = 'Compare keywords algorithm'
