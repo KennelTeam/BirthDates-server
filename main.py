8cb26a0048446e2682f63ef2344441605b2c612e
nilovlev <nilovlev@yandex.ru>
2022-01-10 04:01:45 +0300

    telegram bot final version

--- main.py
+++ main.py
@@ -1,25 +1,6 @@
-from user_questions import user_choose
-from clustering_graph_db_functions import *
-from db_functions import get_product
+from aiogram.utils import executor
 
-initial = get_root_clusters()
-prev = 0
-current = initial
-print([a[0] for a in current])
+from bot import dp, shutdown
 
-while len(current) > 0:
-    index = user_choose([x[1] for x in current])
-    id = current[index][0]
-    prev = id
-    print(id)
-    current = get_child_clusters(id)
-    print([a[0] for a in current])
-
-print(get_cluster_keywords(prev))
-
-products = get_cluster_products(prev)
-
-print(products)
-
-for product in products:
-    print(get_product(product))
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True, on_shutdown=shutdown)
